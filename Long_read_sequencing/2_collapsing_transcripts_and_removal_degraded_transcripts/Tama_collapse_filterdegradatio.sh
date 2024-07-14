073dc30d7516352a01d1f1984e0629d9e2969a0a
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-14 16:51:00 +0200

    Update Tama_collapse_filterdegradatio.sh

--- Long_read_sequencing/2_collapsing_transcripts_and_removal_degraded_transcripts/Tama_collapse_filterdegradatio.sh
+++ Long_read_sequencing/2_collapsing_transcripts_and_removal_degraded_transcripts/Tama_collapse_filterdegradatio.sh
@@ -33,23 +33,8 @@ tama_remove_fragment_models.py -f filterdegradationtama.bed -o filterdegradation
 
 python tama_convert_bed_gtf_ensembl_no_cds.py filterdegradationtama.bed filterdegradationtama.gtf
 
-R
-
-# R code for modifying table
-# the transcript of the bed file needs to be replaced with the PB info
-# Important to use the capped.bed file and not the trans_read.bed cause this one is transcripts before collapsing
-
-library(tidyverse) #not sure that tidyverse is needed here, I loaded for testing similar codes and then I did not try without
-filterdegradationtama<- read.delim("/Beatriz_Toledo/tama-master/filterdegradationtama.gtf", header=FALSE)
-bff2nocap<-filterdegradationtama %>%
-  mutate(V2=str_replace(filterdegradationtama$V2,"PBRI", "PacBio"))%>%
-  mutate(V9=gsub('G', '"PB.', filterdegradationtama$V9)) 
-bff2nocap<-bff2nocap %>%
-  mutate(V9=str_replace(bff2nocap$V9,'exon_number ', 'exon_number "'))
-bff2nocap<-bff2nocap %>%
-  mutate(V9=gsub(';', '";', bff2nocap$V9)) write.table(bff2nocap,"/Beatriz_Toledo/tama-master/filterdegradationtama.modified.gtf", sep="\t", quote= FALSE , row.name=FALSE, col.names = FALSE)
-
-q()
+#Run R script to modify table
+Rscript Modify_Tama_table.r
 
 #get the fasta file from the modified gtf file with gffread
 

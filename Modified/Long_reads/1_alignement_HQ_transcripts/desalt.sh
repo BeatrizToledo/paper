b3eadebdbd041d7eba13ba7542bc3eab8081739b
leilaepfl <98028786+leilaepfl@users.noreply.github.com>
2024-07-20 20:04:44 +0200

    Update and rename deSalt.sh to desalt.sh

--- Modified/Long_reads/1_alignement_HQ_transcripts/desalt.sh
+++ Modified/Long_reads/1_alignement_HQ_transcripts/desalt.sh
@@ -3,8 +3,8 @@
 #load module
 module load apps desalt
 
-#build index
+#build index from reference fasta files
 deSALT index ref.fa <index_route>
 
-#align high-quality reads to the index
+#align high-quality long-reads to the index
 deSALT aln ../mouse/ ../long_reads_fasta/hq_transcripts.fasta --thread 15 --seed-step 3 --min-chain-score 27 --max-intron-len 400000  -x ccs -O6,24 -M4 -o hq.sam

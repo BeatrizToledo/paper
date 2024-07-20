f4730359262c6804e0b7024fe607964f4ab08f64
leilaepfl <98028786+leilaepfl@users.noreply.github.com>
2024-07-20 20:50:32 +0200

    Update desalt.sh

--- Modified/Long_reads/1_alignement_HQ_transcripts/desalt.sh
+++ Modified/Long_reads/1_alignement_HQ_transcripts/desalt.sh
@@ -8,3 +8,6 @@ deSALT index ref.fa <index_route>
 
 #align high-quality long-reads to the index
 deSALT aln ../mouse/ ../long_reads_fasta/hq_transcripts.fasta --thread 15 --seed-step 3 --min-chain-score 27 --max-intron-len 400000  -x ccs -O6,24 -M4 -o hq.sam
+
+#sort with samtools
+hq.sam hq.rn.sorted.sam

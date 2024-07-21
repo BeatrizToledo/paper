2e685b4d7844e6a0f8a5477554586dc24caa61e7
leilaepfl <98028786+leilaepfl@users.noreply.github.com>
2024-07-21 17:38:13 +0200

    Update alignment_all.sh

--- Modified/Alternative splicing/Identify and quantify AS events/alignment_all.sh
+++ Modified/Alternative splicing/Identify and quantify AS events/alignment_all.sh
@@ -11,17 +11,17 @@ STAR --runMode genomeGenerate --genomeDir /../STAR_align_merged_gtf/ --genomeFas
 
 #align short-read paired-end sequencing data (_R1.fastq.gz and _R2.fastq.gz) for each replicate 
 #sample1 (NSC) - 3 replicates
-STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L222 --readFilesIn /../fastq/L222_R1.fastq.gz /../fastq/L222_R2.fastq.gz
-STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L355 --readFilesIn /../fastq/L355_R1.fastq.gz /../fastq/L355_R2.fastq.gz 
-STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L393 --readFilesIn /../fastq/L393_R1.fastq.gz /../fastq/L393_R2.fastq.gz
+STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L222 --readFilesIn /../short-reads/L222_R1.fastq.gz /../short-reads/L222_R2.fastq.gz
+STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L355 --readFilesIn /../short-reads/L355_R1.fastq.gz /../short-reads/L355_R2.fastq.gz 
+STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L393 --readFilesIn /../short-reads/L393_R1.fastq.gz /../short-reads/L393_R2.fastq.gz
 #sample2 (NP) - 3 replicates
-STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L223 --readFilesIn /../fastq/L223_R1.fastq.gz /../fastq/L223_R2.fastq.gz
-STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L356 --readFilesIn /../fastq/L356_R1.fastq.gz /../fastq/L356_R2.fastq.gz
-STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L394 --readFilesIn /../fastq/L394_R1.fastq.gz /../fastq/L394_R2.fastq.gz
+STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L223 --readFilesIn /../short-reads/L223_R1.fastq.gz /../short-reads/L223_R2.fastq.gz
+STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L356 --readFilesIn /../short-reads/L356_R1.fastq.gz /../short-reads/L356_R2.fastq.gz
+STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome  --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L394 --readFilesIn /../short-reads/L394_R1.fastq.gz /../short-reads/L394_R2.fastq.gz
 #sample3 (N) - 3 replicates
-STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome   --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L224 --readFilesIn /../fastq/L224_R1.fastq.gz /../fastq/L224_R2.fastq.gz
-STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome   --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L357 --readFilesIn /../fastq/L357_R1.fastq.gz /../fastq/L357_R2.fastq.gz
-STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome   --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L395 --readFilesIn /../fastq/L395_R1.fastq.gz /../fastq/L395_R2.fastq.gz
+STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome   --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L224 --readFilesIn /../short-reads/L224_R1.fastq.gz /../short-reads/L224_R2.fastq.gz
+STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome   --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L357 --readFilesIn /../short-reads/L357_R1.fastq.gz /../short-reads/L357_R2.fastq.gz
+STAR --runThreadN 16 --genomeDir /../STAR_align_merged_gtf/genome   --readFilesCommand zcat  --quantMode GeneCounts --outSAMtype BAM Unsorted --outFileNamePrefix L395 --readFilesIn /../short-reads/L395_R1.fastq.gz /../short-reads/L395_R2.fastq.gz
 
 #merge single .bam files into a unique one with samtools merge
 samtools merge All.merged.bam \

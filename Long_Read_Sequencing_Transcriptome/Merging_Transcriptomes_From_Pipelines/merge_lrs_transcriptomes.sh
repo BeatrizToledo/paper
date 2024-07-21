9a5ffc5ee68b191dc7519400c60668ed6d3500ff
leilaepfl <98028786+leilaepfl@users.noreply.github.com>
2024-07-21 20:08:39 +0200

    Update merge_lrs_transcriptomes.sh

--- Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/merge_lrs_transcriptomes.sh
+++ Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/merge_lrs_transcriptomes.sh
@@ -4,7 +4,7 @@
 #transcriptomes to be merged are listed in mergelisttama4.txt, result is LRS.gtf
 
 #load modules
-module load apps/python/ ?
+module load apps/python/2.9 #ro check
 
 #merge transcriptome using tama_merge
 python tama_merge.py -f mergelisttama4.txt -p LRS.gtf -a 25 -m 10 -z 10 -d merge_dup

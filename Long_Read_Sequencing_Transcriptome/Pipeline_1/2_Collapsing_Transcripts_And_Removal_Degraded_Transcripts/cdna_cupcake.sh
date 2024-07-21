30b34b88cd88fc8ea609c575fdd4baec298b6242
leilaepfl <98028786+leilaepfl@users.noreply.github.com>
2024-07-21 15:15:22 +0200

    Update and rename code1.sh to cdna_cupcake.sh

---
+++ Long_Read_Sequencing_Transcriptome/Pipeline_1/2_Collapsing_Transcripts_And_Removal_Degraded_Transcripts/cdna_cupcake.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#use with the alignment file from gmap highquality_gmap.sam (from step 1)
+#cDNA Cupcake is run as a singularity image file (.sif) within a Singularity container
+
+#load modules
+module load apps/singularity/3.7.1 
+
+#make directory for output
+mkdir cupcake
+
+#cdna_cupcake collapse
+singularity exec -B /../cupcake /projects/globalscratch/cdna_cupcake.sif collapse_isoforms_by_sam.py --input /../long_reads_fasta/hq_transcripts.fasta  -s  /../GMAP/highquality_gmap_sorted.sam -o  /../cupcake/highquality_gmap_sorted_cup --dun-merge-5-shorter --max_fuzzy_junction=5
+
+#filter for degradation
+singularity exec -B /../cupcake /projects/globalscratch/cdna_cupcake.sif filter_away_subset.py /../cupcake/highquality_gmap_sorted_cup.collapsed

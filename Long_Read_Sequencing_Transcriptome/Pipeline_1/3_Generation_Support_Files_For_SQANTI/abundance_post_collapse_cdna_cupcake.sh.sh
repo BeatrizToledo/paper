5227f2cc7928e8011d540bd19808d3a7e65f4cba
leilaepfl <98028786+leilaepfl@users.noreply.github.com>
2024-07-21 16:03:16 +0200

    Update and rename code1.sh to abundance_post_collapse_cdna_cupcake.sh.sh

---
+++ Long_Read_Sequencing_Transcriptome/Pipeline_1/3_Generation_Support_Files_For_SQANTI/abundance_post_collapse_cdna_cupcake.sh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#use with the collapsed file from gmap+cupcake highquality_gmap_sorted_cup.collapsed
+#cDNA Cupcake is run as a singularity image file (.sif) within a Singularity container
+
+#load modules
+module load apps/singularity/3.7.1 
+
+#get aboundance of the long reads
+singularity exec -B /../SQANTI_INPUT /projects/globalscratch/cdna_cupcake.sif get_abundance_post_collapse.py /../CUPCAKE/highquality_gmap_sorted_cup.collapsed unpolished.cluster_report.csv

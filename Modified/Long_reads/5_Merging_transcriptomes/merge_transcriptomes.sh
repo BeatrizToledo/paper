066a9c445ec7833ffc75c24bb831c7cbc8993d05
leilaepfl <98028786+leilaepfl@users.noreply.github.com>
2024-07-21 10:54:22 +0200

    Update merge_transcriptomes.sh

--- Modified/Long_reads/5_Merging_transcriptomes/merge_transcriptomes.sh
+++ Modified/Long_reads/5_Merging_transcriptomes/merge_transcriptomes.sh
@@ -4,7 +4,7 @@
 #transcriptomes to be merged are listed in mergelisttama4.txt, result is mergedpc2filterdegradationtama.nofus.gtf
 
 #load modules
-module load apps/python
+module load apps/python/ ?
 
 #merge transcriptome using tama_merge
 python tama_merge.py -f mergelisttama4.txt -p mergedpc2filterdegradationtama.nofus.gtf -a 25 -m 10 -z 10 -d merge_dup

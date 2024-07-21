2f755c300315e8f7086a0c44ac63a5d82330677a
leilaepfl <98028786+leilaepfl@users.noreply.github.com>
2024-07-21 15:06:14 +0200

    Update gmap.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_1/1_Alignment/gmap.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_1/1_Alignment/gmap.sh
@@ -2,6 +2,7 @@
 
 #load modules
 module load apps/gmap #version?
+module load apps/samtools/1.9
 
 #make directory for alignment results
 mkdir GMAP
@@ -11,3 +12,6 @@ gmap_build -d GENOME_Assembly -s none -k 15 -D /../GMAP/ Mus_musculus.GRCm38.dna
 
 #align high-quality long-reads to the index
 gmap -D /../GMAP/ -d GENOME_Assembly ../long_reads_fasta/hq_transcripts.fasta --no-chimeras --min-trimmed-coverage 0.85 --min-identity 0.9 -B 5 -t 24 -f samse --cross-species -z sense_force -n 1 -K 400000 > highquality_gmap.sam
+
+#sort .sam for cDNA Cupcake collapse
+samtools sort -o /../GMAP/highquality_gmap_sorted.sam -O sam /../GMAP/highquality_gmap.sam

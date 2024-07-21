12c3976fc2f29abf512d6d0d8ae8da75e49ae80f
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-21 21:49:42 +0200

    Delete Long_read_sequencing/3_generation_support files_for_SQANTI directory

--- Long_read_sequencing/3_generation_support files_for_SQANTI/abundance_post_collapse_cDNA_cupcake.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# get aboundance of the long reads
-
-singularity exec -B /Beatriz_Toledo/gc /projects/globalscratch/cdna_cupcake.sif get_abundance_post_collapse.py highquality_gmap1_sort_cup.collapsed unpolished.cluster_report.csv

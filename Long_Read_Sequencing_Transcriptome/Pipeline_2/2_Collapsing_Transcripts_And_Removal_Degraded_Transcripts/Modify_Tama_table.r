3ccf954a1e64f4c65ec7c4da2714fcb46a74dc7f
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-22 15:42:54 +0200

    Update Modify_Tama_table.r

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/2_Collapsing_Transcripts_And_Removal_Degraded_Transcripts/Modify_Tama_table.r
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/2_Collapsing_Transcripts_And_Removal_Degraded_Transcripts/Modify_Tama_table.r
@@ -4,12 +4,17 @@
 # the transcript of the bed file needs to be replaced with the PB info
 # Important to use the capped.bed file and not the trans_read.bed cause this one is transcripts before collapsing
 
-library(tidyverse) 
-filterdegradationtama<- read.delim("/Beatriz_Toledo/tama-master/filterdegradationtama.gtf", header=FALSE)
-bff2nocap<-filterdegradationtama %>%
+library(stringr) 
+library(dplyr) 
+library(tidyverse)
+library(plyr) 
+ 
+filterdegradationtama<- read.delim("/../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.gtf", header=FALSE)
+filterdegradationtama1 <-filterdegradationtama %>%
   mutate(V2=str_replace(filterdegradationtama$V2,"PBRI", "PacBio"))%>%
   mutate(V9=gsub('G', '"PB.', filterdegradationtama$V9)) 
-bff2nocap<-bff2nocap %>%
-  mutate(V9=str_replace(bff2nocap$V9,'exon_number ', 'exon_number "'))
-bff2nocap<-bff2nocap %>%
-  mutate(V9=gsub(';', '";', bff2nocap$V9)) write.table(bff2nocap,"/Beatriz_Toledo/tama-master/filterdegradationtama.modified.gtf", sep="\t", quote= FALSE , row.name=FALSE, col.names = FALSE)
+filterdegradationtama1 <-filterdegradationtama1 %>%
+  mutate(V9=str_replace(filterdegradationtama1$V9,'exon_number ', 'exon_number "'))
+filterdegradationtama1<-filterdegradationtama1 %>%
+  mutate(V9=gsub(';', '";', filterdegradationtama1$V9)) 
+write.table(filterdegradationtama1,"/../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.gtf", sep="\t", quote= FALSE , row.name=FALSE, col.names = FALSE)

72343aa7d43cf0e051273783fba2d3ab5632bfcc
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-23 16:07:08 +0200

    Update full-length_abundance_post_collapse_desalt_tama.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/full-length_abundance_post_collapse_desalt_tama.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/full-length_abundance_post_collapse_desalt_tama.sh
@@ -13,7 +13,7 @@ module load apps/python/2.7.0
 
 python tama_read_support_levels.py -f /../TAMA/filelist_trans.txt -o /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap -m /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap_trans_read.bed -mt tama
 
-# in R 
+#in R 
 
 Rscript full-length_abundance_post_collapse_desalt_tama.r
 
@@ -25,11 +25,7 @@ export PYTHONPATH=$PYTHONPATH:<path_to_Cupcake>/sequence
 export PATH=$PATH:<path_to_Cupcake>/sequence
 module load apps/python3/3.7.0
 
-# In the command line in the terminal
-
-cat filterdegradationtama_nocap_trans_readsupport_modified.txt | sed 's/tama://g' > filterdegradationtama_nocap_trans_readsupport_modified2.txt
-cat filterdegradationtama_nocap_trans_readsupport_modified2.txt | sed 's/G/PB./g' > filterdegradationtama_nocap_trans_readsupport_modified3.txt
-
+#use .fa file from step 2
 #scripts from cdna_Cupcake repository 
 python /../cDNA_Cupcake/sequence/fa2fq.py /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.fa
-python /../cDNA_Cupcake/post_isoseq_cluster/demux_isoseq_with_genome.py --mapped_fafq  /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.fastq --read_stat filterdegradationtama_nocap_trans_readsupport_modified3.txt --classify_csv flnc.report.csv -o filterdegradationtama.mapped_fl_count.txt
+python /../cDNA_Cupcake/post_isoseq_cluster/demux_isoseq_with_genome.py --mapped_fafq  /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.fastq --read_stat /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered_read_support_modified.txt --classify_csv /../long_reads_fasta/flnc.report.csv -o /../SQANTI_INPUT_DESALT_TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered_full-length_count.txt

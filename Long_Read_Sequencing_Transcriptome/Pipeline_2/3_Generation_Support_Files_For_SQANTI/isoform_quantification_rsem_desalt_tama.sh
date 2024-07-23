129315a7ddb9cf87cd1be23dc98a174940a8a79a
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-23 06:49:00 +0200

    Update isoform_quantification_rsem_desalt_tama.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/isoform_quantification_rsem_desalt_tama.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/isoform_quantification_rsem_desalt_tama.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#calculate isoform expression from short-reads data from the same cell types as for the long-read sequencing,using rsem. Calculate expression for each replicate separately
 #use with the collapsed and filtered file highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.gtf from step 2 modified as specified 
 
 #load modules
@@ -7,8 +8,6 @@ module load apps/rsem/1.3.3
 
 #prepare the reference
 rsem-prepare-reference --num-threads 40 --gtf /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.gtf --star /../TAMA/Mus_musculus.GRCm38.dna.primary_assembly.fa /../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR
-
-#calculate isoform expression from short-reads data from the same cell types as for the long-read sequencing,using rsem. Calculate expression for each replicate separately
 
 #sample1 (NSC) - 3 replicates
 rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L222_R1.fastq /../short-reads/L222_R2.fastq /../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR /../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L222

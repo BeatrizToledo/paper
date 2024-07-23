429f9c1dfc49a2e7265ca0e664aae48f011258ed
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-23 16:36:28 +0200

    Update classification_and_filtering_sqanti3_desalt_tama.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/4_Classification_And_Removal_Artifacts_SQANTI/classification_and_filtering_sqanti3_desalt_tama.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/4_Classification_And_Removal_Artifacts_SQANTI/classification_and_filtering_sqanti3_desalt_tama.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-#code to classify transcripts and filter artifacts using SQANTI3
+#script for classifying transcripts and filtering artifacts using SQANTI3
 #SQANTI3 is run as a singularity image file (.sif) within a Singularity container
-#use with gtf file generated in step 2 from desalt+tama and isoform (.isoform.results) and splice junction (.twopassSJ.out.tab) quantification obtained in step 3
+#use with gtf file generated in step 2 and full-length (full-length_count.txt), isoform (.isoform.results) and splice junction (.twopassSJ.out.tab) quantification obtained in step 3
 #in addition, SQANTI3 uses as inputs reference fasta and gtf, cage_peaks and PolyA_motifs files
 
 #load modules
 module load apps/singularity/3.5.2
 
 #classify transcripts
-singularity exec -B /group/crtd_calegari/People/Beatriz_Toledo/DESALTTIRDTRY/Sqantipreli \
+singularity exec -B /../SQANTI3_DESALT_TAMA \
     /projects/globalscratch/sqanti3_3.0.sif \
-    sqanti3_qc.py filterdegradationtama.modified.gtf \
-    Mus_musculus.GRCm38.101.gtf \
-    Mus_musculus.GRCm38.dna.primary_assembly.fa \
-    --cage_peak 1refTSS_v3.3_mouse_coordinate.mm10.bed \
-    --polyA_motif_list PolyA_motif_List_mm10.txt \ 
+    sqanti3_qc.py /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.gtf \
+    /../SQANTI3_DESALT_TAMA/Mus_musculus.GRCm38.101.gtf \
+    /../SQANTI3_DESALT_TAMA/Mus_musculus.GRCm38.dna.primary_assembly.fa \
+    --cage_peak /../SQANTI3_DESALT_TAMA/refTSS_v3.3_mouse_coordinate.mm10.bed \
+    --polyA_motif_list /../SQANTI3_DESALT_TAMA/PolyA_motif_List_mm10.txt \ 
     --gtf \
     -e filterdegradationtama_nocap_read_PP_DP_N_PP_rsemSTAR_before_SQANTI_L393.isoforms.results,\
-filterdegradationtama_nocap_read_PP_DP_N_PP_rsemSTAR_before_SQANTI_L355.isoforms.results,\
-filterdegradationtama_nocap_read_PP_DP_N_PP_rsemSTAR_before_SQANTI_L222.isoforms.results,\
-filterdegradationtama_nocap_read_PP_DP_N_N_rsemSTAR_before_SQANTI_L395.isoforms.results,\
-filterdegradationtama_nocap_read_PP_DP_N_N_rsemSTAR_before_SQANTI_L357.isoforms.results,\
-filterdegradationtama_nocap_read_PP_DP_N_N_rsemSTAR_before_SQANTI_L224.isoforms.results,\
-filterdegradationtama_nocap_read_PP_DP_N_DP_rsemSTAR_before_SQANTI_L394.isoforms.results,\
-filterdegradationtama_nocap_read_PP_DP_N_DP_rsemSTAR_before_SQANTI_L356.isoforms.results,\
-filterdegradationtama_nocap_read_PP_DP_N_DP_rsemSTAR_before_SQANTI_L223.isoforms.results \
+DT.rsemSTAR_L355.isoforms.results,\
+DT.rsemSTAR_L222.isoforms.results,\
+DT.rsemSTAR_L395.isoforms.results,\
+DT.rsemSTAR_L357.isoforms.results,\
+DT.rsemSTAR_L224.isoforms.results,\
+DT.rsemSTAR_L394.isoforms.results,\
+DT.rsemSTAR_L356.isoforms.results,\
+DT.rsemSTAR_L223.isoforms.results \
     -fl filterdegradationtama.mapped_fl_count.txt -o PP_DP_N_filtereddegradationama_NOCAP_sqanti2 \
     --isoAnnotLite --gff3 Mus_musculus_GRCm38_Ensembl_86.gff3 \
     -c filterdegradationtamaDP_L356_twopassSJ.out.tab,filterdegradationtamaDP_L223_twopassSJ.out.tab,\

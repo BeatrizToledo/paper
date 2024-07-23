6550acc007b61f040618cf258a2256ae1fc8ba4a
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-23 16:11:07 +0200

    Update desalt.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/1_Alignment/desalt.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/1_Alignment/desalt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#script to align high quality long-read transcripts to the genome
+#script for long-read transcripts alignment to the genome
 #load module
 module load apps/desalt/1.5.5
 module load apps/samtools/1.9

da4b0918e5c9773c3e15f24d1b6e7434bf034d8a
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-23 07:03:29 +0200

    Update tama.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/2_Collapsing_Transcripts_And_Removal_Degraded_Transcripts/tama.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/2_Collapsing_Transcripts_And_Removal_Degraded_Transcripts/tama.sh
@@ -41,7 +41,7 @@ python /../tama/tama_go/format_converter/tama_convert_bed_gtf_ensembl_no_cds.py 
 #1       PacBio  exon    3136606 3136668 .       -       .       gene_id "PB.1"; transcript_id "PB.1.1";
 
 #run R script to modify table
-Rscript Modify_Tama_table.r
+Rscript /../TAMA/Modify_Tama_table.r
 
 #get the fasta file from the modified gtf file with gffread
 gffread -w /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.fa -g /../TAMA/Mus_musculus.GRCm38.dna.primary_assembly.fa /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.gtf

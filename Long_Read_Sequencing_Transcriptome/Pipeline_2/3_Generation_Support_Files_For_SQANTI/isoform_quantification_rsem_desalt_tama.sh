385ba4e8ff96fddcd845d7a40b0ea88c9f4743ed
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-23 08:29:48 +0200

    Update isoform_quantification_rsem_desalt_tama.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/isoform_quantification_rsem_desalt_tama.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/isoform_quantification_rsem_desalt_tama.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #script to generate isoform abundance files for SQANTI3
-#use with the collapsed and filtered file highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.gtf from step 2 modified as specified 
+#use with the collapsed and filtered file highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.gtf from step 2
 
 #load modules
 module load apps/rsem/1.3.3

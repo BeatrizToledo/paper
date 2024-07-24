5db2937510f9277c827c53bb0424f114971dcaef
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-24 14:52:41 +0200

    Update cdna_cupcake.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_1/2_Collapsing_Transcripts_And_Removal_Degraded_Transcripts/cdna_cupcake.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_1/2_Collapsing_Transcripts_And_Removal_Degraded_Transcripts/cdna_cupcake.sh
@@ -14,7 +14,7 @@ mkdir CUPCAKE
 #Collapsing step. collapse_isoforms_by_sam.py command generates multiple files, including .gff file.
 singularity exec -B /../CUPCAKE /projects/globalscratch/cdna_cupcake.sif collapse_isoforms_by_sam.py --input /../long_reads_fasta/hq_transcripts.fasta  -s  /../GMAP/highquality_gmap_sorted.sam -o  /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed --dun-merge-5-shorter --max_fuzzy_junction=5
 
-#filter out degraded transcripts
+#filter out degraded transcripts. filter_away_subset.py command generates multiple files, including .gff file.
 singularity exec -B /../CUPCAKE /projects/globalscratch/cdna_cupcake.sif filter_away_subset.py /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed
 
 #make directory for resulst of step 3

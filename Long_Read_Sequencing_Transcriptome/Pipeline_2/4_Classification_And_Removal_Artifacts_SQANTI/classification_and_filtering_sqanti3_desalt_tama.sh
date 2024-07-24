6838cdf61be462f4216b605e28877fdaf5056d52
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-24 23:04:16 +0200

    Update classification_and_filtering_sqanti3_desalt_tama.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/4_Classification_And_Removal_Artifacts_SQANTI/classification_and_filtering_sqanti3_desalt_tama.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/4_Classification_And_Removal_Artifacts_SQANTI/classification_and_filtering_sqanti3_desalt_tama.sh
@@ -6,7 +6,7 @@
 #in addition, SQANTI3 uses as inputs reference fasta and gtf, cage_peaks (refTSS database) and PolyA_motifs (supplied in the SQANTI3 data folder) files
 
 #load modules
-module load apps/singularity/3.5.2
+module load apps/singularity/3.7.1
 
 #classify transcripts
 singularity exec -B /../SQANTI3_DESALT_TAMA,/../TAMA,/../SQANTI_INPUT_DESALT_TAMA \

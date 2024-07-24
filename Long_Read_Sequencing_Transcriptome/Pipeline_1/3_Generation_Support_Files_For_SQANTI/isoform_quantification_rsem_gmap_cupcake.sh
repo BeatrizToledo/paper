adcc9a7523bce915a4d8ada464e84ebbbe1c584e
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-24 22:25:46 +0200

    Update isoform_quantification_rsem_gmap_cupcake.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_1/3_Generation_Support_Files_For_SQANTI/isoform_quantification_rsem_gmap_cupcake.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_1/3_Generation_Support_Files_For_SQANTI/isoform_quantification_rsem_gmap_cupcake.sh
@@ -7,19 +7,19 @@
 module load apps/rsem/1.3.3
 
 #prepare the reference
-rsem-prepare-reference --num-threads 40 --gtf /../CUPCAKE/highquality_gmap_sort_cup.collapsed.filtered.gff --star /../CUPCAKE/Mus_musculus.GRCm38.dna.primary_assembly.fa /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR
+rsem-prepare-reference --num-threads 40 --gtf /../CUPCAKE/highquality_gmap_sort_cup.collapsed.filtered.gff --star /../CUPCAKE/Mus_musculus.GRCm38.dna.primary_assembly.fa /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar
 
 #calculate isoform expression from short-reads data from the same cell types as for the long-read sequencing,using rsem. Calculate expression for each replicate separately
 
 #sample1 (NSC)  - 3 replicates
-rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L222_R1.fastq /../short-reads/L222_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR_L222
-rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L355_R1.fastq /../short-reads/L355_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR_L355
-rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L393_R1.fastq /../short-reads/L393_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR_L393
+rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L222_R1.fastq /../short-reads/L222_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar_L222
+rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L355_R1.fastq /../short-reads/L355_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar_L355
+rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L393_R1.fastq /../short-reads/L393_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar_L393
 #sample2 (NP) - 3 replicates
-rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L223_R1.fastq /../short-reads/L223_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR_L223
-rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L356_R1.fastq /../short-reads/L356_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR_L356
-rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L394_R1.fastq /../short-reads/L394_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR_L394
+rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L223_R1.fastq /../short-reads/L223_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar_L223
+rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L356_R1.fastq /../short-reads/L356_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar_L356
+rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L394_R1.fastq /../short-reads/L394_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar_L394
 #sample3 (N) - 3 replicates
-rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L224_R1.fastq /../short-reads/L224_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR_L224
-rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L357_R1.fastq /../short-reads/L357_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR_L357
-rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L395_R1.fastq /../short-reads/L395_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR /../SQANTI_INPUT_GMAP_CUPCAKE/GC.rsemSTAR_L395
+rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L224_R1.fastq /../short-reads/L224_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar_L224
+rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L357_R1.fastq /../short-reads/L357_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar_L357
+rsem-calculate-expression --paired-end --strand-specific --star --num-threads 45 --no-bam-output /../short-reads/L395_R1.fastq /../short-reads/L395_R2.fastq /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar /../SQANTI_INPUT_GMAP_CUPCAKE/gc.rsemstar_L395

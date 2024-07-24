d710bd53f3e36ce2c49cc97ca6f3d7e978d98f77
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-24 14:45:31 +0200

    Update cdna_cupcake.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_1/2_Collapsing_Transcripts_And_Removal_Degraded_Transcripts/cdna_cupcake.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_1/2_Collapsing_Transcripts_And_Removal_Degraded_Transcripts/cdna_cupcake.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#use with the alignment file from gmap highquality_gmap.sam (from step 1)
+#script for collapsing redundant transcripts and removing potentially degraded transcripts
+#use with the alignment file from gmap highquality_gmap_sorted.sam (from step 1)
 #cDNA Cupcake is run as a singularity image file (.sif) within a Singularity container
 
 #load modules
@@ -10,11 +11,11 @@ module load apps/cupcake/12.0.0
 #make directory for output
 mkdir CUPCAKE
 
-#cdna_cupcake collapse
-singularity exec -B /../CUPCAKE /projects/globalscratch/cdna_cupcake.sif collapse_isoforms_by_sam.py --input /../long_reads_fasta/hq_transcripts.fasta  -s  /../GMAP/highquality_gmap_sorted.sam -o  /../CUPCAKE/highquality_gmap_sorted_cup --dun-merge-5-shorter --max_fuzzy_junction=5
+#Collapsing step. collapse_isoforms_by_sam.py command generates multiple files, including .gff file.
+singularity exec -B /../CUPCAKE /projects/globalscratch/cdna_cupcake.sif collapse_isoforms_by_sam.py --input /../long_reads_fasta/hq_transcripts.fasta  -s  /../GMAP/highquality_gmap_sorted.sam -o  /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed --dun-merge-5-shorter --max_fuzzy_junction=5
 
-#filter for degradation
-singularity exec -B /../CUPCAKE /projects/globalscratch/cdna_cupcake.sif filter_away_subset.py /../CUPCAKE/highquality_gmap_sorted_cup.collapsed
+#filter out degraded transcripts
+singularity exec -B /../CUPCAKE /projects/globalscratch/cdna_cupcake.sif filter_away_subset.py /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed
 
 #make directory for resulst of step 3
 mkdir SQANTI_INPUT_GMAP_CUPCAKE

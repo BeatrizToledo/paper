b4f7f7be77c588630e20884a8e446e700becf94a
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-24 17:06:45 +0200

    Update full-length_abundance_post_collapse_desalt_tama.r

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/full-length_abundance_post_collapse_desalt_tama.r
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/full-length_abundance_post_collapse_desalt_tama.r
@@ -3,7 +3,7 @@
 #R code for modifying table
 #generate an file that associates m…/../ with PB. and with collumns id length is_fl stat pbid
 #m64012_190727_053041/105120134/ccs   NA   Y   unique   PB.16.1
-#use with filterd file highquality_desalt_sorted_tama.collapsed_nocap.filtered.gtf 
+#use with filtered file highquality_desalt_sorted_tama.collapsed_nocap.filtered.gtf to obtain the list of transcripts after filtering
 # 1       PBRI  transcript        3199736 3202443 .       -       .       gene_id "G1.1"; transcript_id "transcript/132038"; uniq_trans_id "transcript/132038";
 #use with highquality_desalt_sorted_tama.collapsed_nocap_read_support.txt generated from tama_read_support_levels.py command
 #merge_gene_id   merge_trans_id  gene_read_count trans_read_count        source_line     support_line

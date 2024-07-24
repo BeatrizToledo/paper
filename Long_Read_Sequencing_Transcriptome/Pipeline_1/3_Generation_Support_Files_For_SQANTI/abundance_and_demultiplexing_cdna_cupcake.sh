9492e93a2b8f0232ef7414988922267b13c909a1
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-24 21:04:17 +0200

    Update abundance_and_demultiplexing_cdna_cupcake.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_1/3_Generation_Support_Files_For_SQANTI/abundance_and_demultiplexing_cdna_cupcake.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_1/3_Generation_Support_Files_For_SQANTI/abundance_and_demultiplexing_cdna_cupcake.sh
@@ -5,7 +5,10 @@
 #cDNA Cupcake is run as a singularity image file (.sif) within a Singularity container
 
 #load modules
+#make sure you have a git clone of cDNA_Cupcake repository 
 module load apps/singularity/3.7.1 
+module load apps/gffread/0.12.1
+module load apps/python3/3.7.0
 
 #get the fasta file from .gtf file with gffread
 gffread -w /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.fasta -g /../CUPCAKE/Mus_musculus.GRCm38.dna.primary_assembly.fa /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.gff
@@ -16,14 +19,15 @@ gffread -w /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.fasta -g /../CU
 singularity exec -B /../CUPCAKE /projects/globalscratch/cdna_cupcake.sif get_abundance_post_collapse.py /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed /../long_reads_fasta/unpolished.cluster_report.csv
 
 #use .fa file
-python fa2fq.py /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.fasta
+#make sure you have git clone https://github.com/Magdoll/cDNA_Cupcake.git
+python /../cDNA_Cupcake/sequence/fa2fq.py /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.fasta
 
 #demultiplexing. mapped_fl_count.txt file contains full-length read counts for each mapped, unique, isoform for each sample
 #flnc.report.csv was obtained during Isoseq3 processing
-python demux_isoseq_with_genome.py --mapped_fafq  /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.fastq --read_stat /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.read_stat.txt --classify_csv /../long_reads_fasta/flnc.report.csv -o /../SQANTI_INPUT_GMAP_CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.mapped_fl_count.txt
+python /../cDNA_Cupcake/post_isoseq_cluster/demux_isoseq_with_genome.py --mapped_fafq  /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.fastq --read_stat /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.read_stat.txt --classify_csv /../long_reads_fasta/flnc.report.csv -o /../SQANTI_INPUT_GMAP_CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.mapped_fl_count.txt
 
 #generate demultiplexed GFF and FASTA/FASTQ files 
-python demux_by_barcode_groups.py /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.gff /../SQANTI_INPUT_GMAP_CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.mapped_fl_count.txt /../SQANTI_INPUT_GMAP_CUPCAKE/output_demux_highquality_gmap_sorted_cupcake.collapsed "('bc1001_5p--bc1001_3p','NSC'),('bc1002_5p--bc1002_3p','NP'),('bc1003_5p--bc1003_3p','N’)”
+python /../cDNA_Cupcake/post_isoseq_cluster/demux_by_barcode_groups.py /../CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.gff /../SQANTI_INPUT_GMAP_CUPCAKE/highquality_gmap_sorted_cupcake.collapsed.mapped_fl_count.txt /../SQANTI_INPUT_GMAP_CUPCAKE/output_demux_highquality_gmap_sorted_cupcake.collapsed "('bc1001_5p--bc1001_3p','NSC'),('bc1002_5p--bc1002_3p','NP'),('bc1003_5p--bc1003_3p','N’)”
 
 
 

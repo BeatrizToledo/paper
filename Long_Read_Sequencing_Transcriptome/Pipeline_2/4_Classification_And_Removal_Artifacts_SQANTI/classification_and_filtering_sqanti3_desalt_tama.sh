08b633ce5a31b2123697b40c4e8fbd9ee39a994f
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-24 22:26:34 +0200

    Update classification_and_filtering_sqanti3_desalt_tama.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/4_Classification_And_Removal_Artifacts_SQANTI/classification_and_filtering_sqanti3_desalt_tama.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/4_Classification_And_Removal_Artifacts_SQANTI/classification_and_filtering_sqanti3_desalt_tama.sh
@@ -9,7 +9,7 @@
 module load apps/singularity/3.5.2
 
 #classify transcripts
-singularity exec -B /../SQANTI3_DESALT_TAMA \
+singularity exec -B /../SQANTI3_DESALT_TAMA,/../TAMA,/../SQANTI_INPUT_DESALT_TAMA \
     /projects/globalscratch/sqanti3_3.0.sif \
     sqanti3_qc.py /../TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered.modified.gtf \
     /../SQANTI_INPUT_DESALT_TAMA/Mus_musculus.GRCm38.101.gtf \
@@ -17,26 +17,26 @@ singularity exec -B /../SQANTI3_DESALT_TAMA \
     --cage_peak /../SQANTI_INPUT_DESALT_TAMA/refTSS_v3.3_mouse_coordinate.mm10.bed \
     --polyA_motif_list /../SQANTI_INPUT_DESALT_TAMA/PolyA_motif_List_mm10.txt \ 
     --gtf \
-    -e /../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L393.isoforms.results,\
-/../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L355.isoforms.results,\
-/../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L222.isoforms.results,\
-/../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L395.isoforms.results,\
-/../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L357.isoforms.results,\
-/../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L224.isoforms.results,\
-/../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L394.isoforms.results,\
-/../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L356.isoforms.results,\
-/../SQANTI_INPUT_DESALT_TAMA/DT.rsemSTAR_L223.isoforms.results \
-    -fl /../SQANTI_INPUT_DESALT_TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered_full-length_count.txt -o /../SQANTI_DESALT_TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered_sqanti \
+    -e /../SQANTI_INPUT_DESALT_TAMA/dt.rsemstar_L393.isoforms.results,\
+/../SQANTI_INPUT_DESALT_TAMA/dt.rsemstar_L355.isoforms.results,\
+/../SQANTI_INPUT_DESALT_TAMA/dt.rsemstar_L222.isoforms.results,\
+/../SQANTI_INPUT_DESALT_TAMA/dt.rsemstar_L395.isoforms.results,\
+/../SQANTI_INPUT_DESALT_TAMA/dt.rsemstar_L357.isoforms.results,\
+/../SQANTI_INPUT_DESALT_TAMA/dt.rsemstar_L224.isoforms.results,\
+/../SQANTI_INPUT_DESALT_TAMA/dt.rsemstar_L394.isoforms.results,\
+/../SQANTI_INPUT_DESALT_TAMA/dt.rsemstar_L356.isoforms.results,\
+/../SQANTI_INPUT_DESALT_TAMA/dt.rsemstar_L223.isoforms.results \
+    -fl /../SQANTI_INPUT_DESALT_TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered_full-length_count.txt -o /../SQANTI_DESALT_TAMA/LRS2 \
     --isoAnnotLite --gff3 /../SQANTI_INPUT_DESALT_TAMA/Mus_musculus_GRCm38_Ensembl_86.gff3 \
-    -c /../SQANTI_INPUT_DESALT_TAMA/L356_twopassDTSJ.out.tab,/../SQANTI_INPUT_DESALT_TAMA/L223_twopassDTSJ.out.tab,\
-/../SQANTI_INPUT_DESALT_TAMA/L394_twopassDTSJ.out.tab,/../SQANTI_INPUT_DESALT_TAMA/L357_twopassDTSJ.out.tab,\
-/../SQANTI_INPUT_DESALT_TAMA/L224_twopassDTSJ.out.tab,/../SQANTI_INPUT_DESALT_TAMA/L395_twopassDTSJ.out.tab,\
-/../SQANTI_INPUT_DESALT_TAMA/L355_twopassDTSJ.out.tab,/../SQANTI_INPUT_DESALT_TAMA/L222_twopassDTSJ.out.tab,\
-/../SQANTI_INPUT_DESALT_TAMA/L393_twopassDTSJ.out.tab
+    -c /../SQANTI_INPUT_DESALT_TAMA/L356_dt_sj.out.tab,/../SQANTI_INPUT_DESALT_TAMA/L223_dt_sj.out.tab,\
+/../SQANTI_INPUT_DESALT_TAMA/L394_dt_sj.out.tab,/../SQANTI_INPUT_DESALT_TAMA/L357_dt_sj.out.tab,\
+/../SQANTI_INPUT_DESALT_TAMA/L224_dt_sj.out.tab,/../SQANTI_INPUT_DESALT_TAMA/L395_dt_sj.out.tab,\
+/../SQANTI_INPUT_DESALT_TAMA/L355_dt_sj.out.tab,/../SQANTI_INPUT_DESALT_TAMA/L222_dt_sj.out.tab,\
+/../SQANTI_INPUT_DESALT_TAMA/L393_dt_sj.out.tab
 
 #filter out artifacts and degraded transcripts
 singularity exec -B /../SQANTI3_DESALT_TAMA \
     /projects/globalscratch/sqanti3_3.0.sif \
-    python /../SQANTI_DESALT_TAMA/sqanti3_RulesFilter.pyhighquality_desalt_sorted_tama.collapsed_nocap.filtered_sqanti_classification.txt \
-    /../SQANTI_DESALT_TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered_sqanti_corrected.fasta \
-    /../SQANTI_DESALT_TAMA/highquality_desalt_sorted_tama.collapsed_nocap.filtered_sqanti_corrected.gtf
+    python /../SQANTI_DESALT_TAMA/sqanti3_RulesFilter.py /SQANTI_DESALT_TAMA/LRS2_classification.txt \
+    /../SQANTI_DESALT_TAMA/LRS2_corrected.fasta \
+    /../SQANTI_DESALT_TAMA/LRS2_corrected.gtf

8970a310cbb644cf21e3384c597d7ad21e0f5e2d
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-25 15:29:43 +0200

    Update merge_lrs_transcriptomes.sh

--- Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/merge_lrs_transcriptomes.sh
+++ Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/merge_lrs_transcriptomes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#code to merge the transcriptome obtained from pipeline_1 (LRS_1.gtf) and pipeline_2 (LRS_2.gtf)
+#script to merge the transcriptome obtained from pipeline_1 (LRS_1_classification.filtered_lite.gtf) and pipeline_2 (LRS_2_classification.filtered_lite.gtf)
 #transcriptomes to be merged are listed in mergelisttama4.txt, result is LRS.gtf
 
 #load modules
-module load apps/python/2.9 #to check
+module load apps/python/2.7.0 #to check
 
 #merge transcriptome using tama_merge
 python tama_merge.py -f mergelistlrs.txt -p LRS.gtf -a 25 -m 10 -z 10 -d merge_dup

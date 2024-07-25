a7495a1941788c5a00f2f5f412af12af51c6616f
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-25 16:53:04 +0200

    Update merge_lrs_transcriptomes.sh

--- Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/merge_lrs_transcriptomes.sh
+++ Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/merge_lrs_transcriptomes.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 #script to merge the transcriptome obtained from pipeline_1 (LRS_1_classification.filtered_lite.gtf) and pipeline_2 (LRS_2_classification.filtered_lite.gtf)
-#transcriptomes to be merged are listed in mergelisttama4.txt, result is LRS.gtf
+#mergelistlrs.txt contains name of transcriptome files, if samples were capped, their merging priority and the source name
 
 #load modules
-module load apps/python/2.7.0 #to check
+module load apps/python/2.7.0
+
+#make directory for output
+mkdir MERGED_TRANSCRIPTOME
+
+#convert .gtf files into .bed files
+python tama_format_gff_to_bed12_cupcake.py  PP_DP_N_filtereddegradationama_NOCAP_sqanti2_classification.filtered_lite.gtf  PP_DP_N_filtereddegradationama_NOCAP_sqanti2_classification.filtered_lite_nofus.gtf.bed
 
 #merge transcriptome using tama_merge
-python tama_merge.py -f mergelistlrs.txt -p LRS.gtf -a 25 -m 10 -z 10 -d merge_dup
+python /../tama/tama_merge.py -f /../MERGED_TRANSCRIPTOME/mergelistlrs.txt -p /../MERGED_TRANSCRIPTOME/LRS -a 25 -m 10 -z 10 -d merge_dup
+
+python tama_convert_bed_gtf_ensembl_no_cds.py mergedpc2filterdegradationtama.nofus.gtf.bed mergedpc2filtereddegradationama.nofus.gtf

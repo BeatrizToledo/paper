eec904468a95cc39c96f6b8550ba1bb1265f6927
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-25 16:57:51 +0200

    Update merge_lrs_transcriptomes.sh

--- Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/merge_lrs_transcriptomes.sh
+++ Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/merge_lrs_transcriptomes.sh
@@ -10,9 +10,9 @@ module load apps/python/2.7.0
 mkdir MERGED_TRANSCRIPTOME
 
 #convert .gtf files into .bed files
-python tama_format_gff_to_bed12_cupcake.py  PP_DP_N_filtereddegradationama_NOCAP_sqanti2_classification.filtered_lite.gtf  PP_DP_N_filtereddegradationama_NOCAP_sqanti2_classification.filtered_lite_nofus.gtf.bed
+python /../tama/tama_go/format_converter/tama_format_gff_to_bed12_cupcake.py  PP_DP_N_filtereddegradationama_NOCAP_sqanti2_classification.filtered_lite.gtf  PP_DP_N_filtereddegradationama_NOCAP_sqanti2_classification.filtered_lite_nofus.gtf.bed
 
 #merge transcriptome using tama_merge
 python /../tama/tama_merge.py -f /../MERGED_TRANSCRIPTOME/mergelistlrs.txt -p /../MERGED_TRANSCRIPTOME/LRS -a 25 -m 10 -z 10 -d merge_dup
 
-python tama_convert_bed_gtf_ensembl_no_cds.py mergedpc2filterdegradationtama.nofus.gtf.bed mergedpc2filtereddegradationama.nofus.gtf
+python /..tama/tama_go/format_converter/tama_convert_bed_gtf_ensembl_no_cds.py mergedpc2filterdegradationtama.nofus.gtf.bed mergedpc2filtereddegradationama.nofus.gtf

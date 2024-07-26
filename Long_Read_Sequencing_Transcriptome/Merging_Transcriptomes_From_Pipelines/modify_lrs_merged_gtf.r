30f9c8d9010efeec6d2e7f67611e37317e8b672a
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-26 14:35:32 +0200

    Update modify_lrs_merged_gtf.r

--- Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/modify_lrs_merged_gtf.r
+++ Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/modify_lrs_merged_gtf.r
@@ -11,4 +11,4 @@ LRS_merged.gtf <- read.delim("/../MERGED_TRANSCRIPTOME/LRS_merged.gtf", header=F
 LRS_merged_modified.gtf<-LRS_merged.gtf %>%
   mutate(V2=str_replace(LRS_merged.gtf$V2,"PBRI", "PacBio"))%>%
   mutate(V9=gsub('"G', '"PB.', LRS_merged.gtf$V9)) 
-write.table(LRS_merged_modified.gtf,"/../MERGED_TRANSCRIPTOME/LRS_merged_modified.gtf", sep="\t", quote= FALSE , row.name=FALSE, col.names = FALSE)
+write.table(LRS_merged_modified.gtf,"/../MERGED_TRANSCRIPTOME/LRS.gtf", sep="\t", quote= FALSE , row.name=FALSE, col.names = FALSE)

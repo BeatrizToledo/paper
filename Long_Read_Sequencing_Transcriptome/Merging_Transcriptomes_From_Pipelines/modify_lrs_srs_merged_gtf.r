ee46c11f7dbab6c127ad896f74cc12ce3a4fdae7
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-26 10:13:50 +0200

    Update and rename pending2 to modify_lrs_srs_merged_gtf.r

---
+++ Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/modify_lrs_srs_merged_gtf.r
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+
+#R code for modifying .gtf
+#should be in folder /../MERGED_TRANSCRIPTOME/
+#the transcript G. needs to be replaced with the PB info
+
+library(tidyverse)
+library(dplyr) 
+
+lrs_srs_merged.gtf <- read.delim("/../MERGED_TRANSCRIPTOME/lrs_srs_merged.gtf", header=FALSE,  quote="'")
+lrs_srs_merged_modified.gtf<-lrs_srs_merged.gtf %>%
+  mutate(V2=str_replace(lrs_srs_merged.gtf$V2,"PBRI", "PacBio"))%>%
+  mutate(V9=gsub('"G', '"PB.', lrs_srs_merged.gtf$V9)) 
+write.table(lrs_srs_merged_modified.gtf,"/../MERGED_TRANSCRIPTOME/lrs_srs_merged_modified.gtf", sep="\t", quote= FALSE , row.name=FALSE, col.names = FALSE)

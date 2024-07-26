60eaef751f9506f688c12f53ae0b564070c3c58c
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-26 14:44:04 +0200

    Update modify_lrs_srs_merged_gtf.r

--- Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/modify_lrs_srs_merged_gtf.r
+++ Long_Read_Sequencing_Transcriptome/Merging_Transcriptomes_From_Pipelines/modify_lrs_srs_merged_gtf.r
@@ -7,8 +7,8 @@
 library(tidyverse)
 library(dplyr) 
 
-LRS_SRS_merged.gtf <- read.delim("/../MERGED_TRANSCRIPTOME/LRS_SRS_merged.gtf", header=FALSE,  quote="'")
-LRS_SRS_merged_modified.gtf<-LRS_SRS_merged.gtf %>%
-  mutate(V2=str_replace(LRS_SRS_merged.gtf$V2,"PBRI", "PacBio"))%>%
-  mutate(V9=gsub('"G', '"PB.', LRS_SRS_merged.gtf$V9)) 
-write.table(LRS_SRS_merged_modified.gtf,"/../MERGED_TRANSCRIPTOME/LRS_SRS_merged_modified.gtf", sep="\t", quote= FALSE , row.name=FALSE, col.names = FALSE)
+LRS_SRS.gtf <- read.delim("/../MERGED_TRANSCRIPTOME/LRS_SRS.gtf", header=FALSE,  quote="'")
+LRS_SRS.gtf<-LRS_SRS.gtf %>%
+  mutate(V2=str_replace(LRS_SRS.gtf$V2,"PBRI", "PacBio"))%>%
+  mutate(V9=gsub('"G', '"PB.', LRS_SRS.gtf$V9)) 
+write.table(LRS_SRS.gtf,"/../MERGED_TRANSCRIPTOME/LRS_SRS.gtf", sep="\t", quote= FALSE , row.name=FALSE, col.names = FALSE)

58fc9cdcad8d2931dd65e72a40ba58f8c59a4dff
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-26 17:10:01 +0200

    Create remove_fusion_trancript.r

---
+++ LRS+SRS_Transcriptome/remove_fusion_trancript.r
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+#R code for removing fusion trancripts
+#should be in folder /../MERGED_TRANSCRIPTOME/
+
+library()
+
+LRS_SRS <- read.delim("/../MERGED_TRANSCRIPTOME/LRS_SRS_classification.txt", header=FALSE,  quote="'")
+LRS_SRS_nofusion_classification <- LRS_SRS %>% subset(structural_category != "fusion", select = c("isoform", "structural_category"))  
+write.table(LRS_SRS_nofusion_classification,"/../MERGED_TRANSCRIPTOME/LRS_SRS_nofusion_classification.txt", sep="\t", quote= FALSE , row.name=FALSE, col.names = FALSE)

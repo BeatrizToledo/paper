2340d7f2bc7fc7bfce4569eaa6f4205462c82f06
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-26 11:15:35 +0200

    Update gmap.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_1/1_Alignment/gmap.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_1/1_Alignment/gmap.sh
@@ -9,7 +9,7 @@ module load apps/samtools/1.9
 mkdir GMAP
 
 #build index assembly from reference fasta file
-gmap_build -d GENOME_Assembly -s none -k 15 -D /../GMAP/ Mus_musculus.GRCm38.dna.primary_assembly.fa
+gmap_build -d GENOME_Assembly -s none -k 15 -D /../GMAP/ /../REFERENCE_INPUT/Mus_musculus.GRCm38.dna.primary_assembly.fa
 
 #align high-quality long-reads to the index
 gmap -D /../GMAP/ -d GENOME_Assembly /../long_reads_fasta/hq_transcripts.fasta --no-chimeras --min-trimmed-coverage 0.85 --min-identity 0.9 -B 5 -t 24 -f samse --cross-species -z sense_force -n 1 -K 400000 > /../GMAP/highquality_gmap.sam

0ab6807e0fa6b5be806beac65cec44a71d0618fd
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-28 23:15:52 +0200

    Update bedtools_exclusion.sh

--- Alternative_Splicing_Analysis/Identification_Of_Transcripts_From_AS_Events/bedtools_exclusion.sh
+++ Alternative_Splicing_Analysis/Identification_Of_Transcripts_From_AS_Events/bedtools_exclusion.sh
@@ -2,7 +2,7 @@
 
 #script for check which AS events coordinates is located between the "genomic_start_coord" and "genomic_end_coord" from junction file with corresponding isoform
 
-bedtools intersect -a LRS_SRS_alljncmorethan3_coord.txt -b NODES_coord.txt -wo -s -F 0.99 -filenames > LRS_SRS_excl_event_tr.txt
-bedtools intersect -a ensembl_alljncmorethan3_coord.txt -b NODES_coord.txt -wo -s -F 0.99 -filenames > ensembl_excl_event_tr.txt
-bedtools intersect -a refseq_alljncmorethan3_coord.txt -b NODES_coord.txt -wo -s -F 0.99 -filenames > refseq_excl_event_tr.txt
+bedtools intersect -a /../ISOFORM/LRS_SRS_alljncmorethan3_coord.txt -b /../ISOFORM/NODES_coord.txt -wo -s -F 0.99 -filenames > /../ISOFORM/LRS_SRS_excl_event_tr.txt
+bedtools intersect -a /../ISOFORM/ensembl_alljncmorethan3_coord.txt -b /../ISOFORM/NODES_coord.txt -wo -s -F 0.99 -filenames > /../ISOFORM/ensembl_excl_event_tr.txt
+bedtools intersect -a /../ISOFORM/refseq_alljncmorethan3_coord.txt -b /../ISOFORM/NODES_coord.txt -wo -s -F 0.99 -filenames > /../ISOFORM/refseq_excl_event_tr.txt
 

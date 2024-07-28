a904bf12b55f7b5d176856006fd5c4be0344ce36
leilaepfl <98028786+leilaepfl@users.noreply.github.com>
2024-07-28 23:38:17 +0200

    Update remove_low_quality_transcripts.sh

--- Short_Read_Sequencing_Transcriptome/remove_low_quality_transcripts.sh
+++ Short_Read_Sequencing_Transcriptome/remove_low_quality_transcripts.sh
@@ -3,13 +3,13 @@
 # Assembled transcripts might have artefacts or be low quality.
 # Here some clean up to make before proceeding with SQANTI analysis.
 
-# Step 1: Compare with mm10 and annotate the genes according to Ensembl
-GFFCOMPARE /../ASSEMBLY/SRS.gtf -r SRS.gtf -R -M -o /../GFFCOMPARE/SRS -V
+# compare with mm10 and annotate the genes according to Ensembl
+gffcompare /../ASSEMBLY/SRS.gtf -r SRS.gtf -R -M -o /../GFFCOMPARE/SRS -V
 
-# Step 2: Extract bad class code transcripts with awk from the annotated one
+# extract bad class code transcripts with awk from the annotated one
 awk '{if($0~"class_code \"(s|e|x|p)\""){print $0}}' /../GFFCOMPARE/SRS.annotated.gtf > /../GFFCOMPARE/SRS.annotated.bad.tx.gtf
 
-# Step 3: Run R script to process the GTF files
+# run R script to remove bad quality transcripts and other
 Rscript --vanilla << EOF
 
 # Load required libraries
@@ -18,32 +18,31 @@ library(stringr)
 library(tidyr)
 library(splitstackshape)
 
-# Read the bad transcripts GTF file
+#read the GTF file with annotated bad ranscripts
 SRS.annotated.bad.tx.gtf <- read.delim("/../GFFCOMPARE/SRS.annotated.bad.tx.gtf", header=FALSE)
 
-# Manipulate the data
 gtf.bad <- SRS.annotated.bad.tx.gtf %>% 
   mutate(V9 = str_replace(V9, "\\;", "|")) %>% 
   separate(V9, into = c("transcript_id", "rest"), sep = "\\|")
 
-# Read the original GTF file
+# read the original GTF file
 gtf <- read.delim("/../ASSEMBLY/SRS.gtf", header=FALSE)
 gtf$ID <- paste(gtf$V9)
 
-# Split concatenated strings into multiple columns
+# split concatenated strings into multiple columns
 gtf <- cSplit(gtf, "ID", sep= ";", type.convert=FALSE)
 
-# Subset the data to remove bad transcripts
+# subset the data to remove bad transcripts
 gtf.good <- subset(gtf, !(gtf$ID_2 %in% gtf.bad$transcript_id))
 
-# Remove transcripts with only 1 bp length
+# remove transcripts with only 1 bp length
 unibp <- subset(gtf.good, V4 == V5)
 gtf.good <- subset(gtf.good, !(gtf.good$ID_2 %in% unibp$ID_2))
 
-# Remove the transcripts with "."
+# remove the transcripts with "."
 gtf.good <- subset(gtf.good, V7 == "+" | V7 == "-")
 
-# Write the filtered data to a file
+# write the filtered data to a file
 write.table(gtf.good, "/../ASSEMBLY/SRS.gtf", sep="\t", row.names = FALSE, col.names=FALSE, quote=FALSE)
 
 EOF

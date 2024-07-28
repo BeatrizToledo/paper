130e289be2147de20bd53b5bc2ab2127369c3ad8
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-28 16:10:47 +0200

    Update junction_coverage_star_desalt_tama.sh

--- Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/junction_coverage_star_desalt_tama.sh
+++ Long_Read_Sequencing_Transcriptome/Pipeline_2/3_Generation_Support_Files_For_SQANTI/junction_coverage_star_desalt_tama.sh
@@ -12,14 +12,14 @@ STAR --runThreadN 65 --runMode genomeGenerate --genomeDir /../SQANTI_INPUT_DESAL
 #calculate junction coverage for each cell type from short-read sequencing data using STAR. Calculate expression for each replicate separately
 #will generate multiple files. SJ.out.tab file will be used for SQANTI3
 #sample1 (NSC)  - 3 replicates
-STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../short-reads/L222_R1.fastq /../short-reads/L222_R2.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L222_twopassDT --outSAMtype None --twopassMode Basic
-STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../short-reads/L355_R1.fastq /../short-reads/L355_R2.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L355_twopassDT --outSAMtype None --twopassMode Basic
-STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../short-reads/L393_R1.fastq /../short-reads/L393_R2.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L393_twopassDT --outSAMtype None --twopassMode Basic
+STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../SHORT-READS/L222_R1.fastq /../SHORT-READS/L222_R2.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L222_twopassDT --outSAMtype None --twopassMode Basic
+STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../SHORT-READS/L355_R1.fastq /../SHORT-READS/L355_R2.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L355_twopassDT --outSAMtype None --twopassMode Basic
+STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../SHORT-READS/L393_R1.fastq /../SHORT-READS/L393_R2.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L393_twopassDT --outSAMtype None --twopassMode Basic
 #sample2 (NP) - 3 replicates
-STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../short-reads/L223_R2.fastq /../short-reads/L223_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L223_twopassDT --outSAMtype None --twopassMode Basic
-STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../short-reads/L356_R2.fastq /../short-reads/L356_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L356_twopassDT --outSAMtype None --twopassMode Basic
-STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../short-reads/L394_R2.fastq /../short-reads/L394_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L394_twopassDT --outSAMtype None --twopassMode Basic
+STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../SHORT-READS/L223_R2.fastq /../SHORT-READS/L223_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L223_twopassDT --outSAMtype None --twopassMode Basic
+STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../SHORT-READS/L356_R2.fastq /../SHORT-READS/L356_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L356_twopassDT --outSAMtype None --twopassMode Basic
+STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../SHORT-READS/L394_R2.fastq /../SHORT-READS/L394_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L394_twopassDT --outSAMtype None --twopassMode Basic
 #sample3 (N) - 3 replicates
-STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../short-reads/L224_R2.fastq /../short-reads/L224_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L224_twopassDT --outSAMtype None --twopassMode Basic
-STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../short-reads/L357_R2.fastq /../short-reads/L357_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L357_twopassDT --outSAMtype None --twopassMode Basic
-STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../short-reads/L395_R2.fastq /../short-reads/L395_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L395_twopassDT --outSAMtype None --twopassMode Basic
+STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../SHORT-READS/L224_R2.fastq /../SHORT-READS/L224_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L224_twopassDT --outSAMtype None --twopassMode Basic
+STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../SHORT-READS/L357_R2.fastq /../SHORT-READS/L357_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L357_twopassDT --outSAMtype None --twopassMode Basic
+STAR --runThreadN 65 --genomeDir /../SQANTI_INPUT_DESALT_TAMA/  --readFilesIn /../SHORT-READS/L395_R2.fastq /../SHORT-READS/L395_R1.fastq --outFileNamePrefix /../SQANTI_INPUT_DESALT_TAMA/L395_twopassDT --outSAMtype None --twopassMode Basic

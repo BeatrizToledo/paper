5e825862edce6621b6f8a362a08b89bedbe0e952
BeatrizToledo <54524859+BeatrizToledo@users.noreply.github.com>
2024-07-29 21:15:50 +0200

    Update Inclusion_Isoforms.sh

--- Alternative_Splicing_Analysis/Identification_Of_Transcripts_From_AS_Events/Inclusion_Isoforms.sh
+++ Alternative_Splicing_Analysis/Identification_Of_Transcripts_From_AS_Events/Inclusion_Isoforms.sh
@@ -28,25 +28,52 @@ keeping.order <- function(data, fn, ...) {
   out 
 }
 
-#filter out low expressed isoforms (less thant 4 reads in at least 1 splice-junction)
+#filter out low expressed isoforms (less thant 4 reads in at least 1 splice-junction) from LRS_SRS
 
 #table with splice junction coverage information
 LRS_SRS_junctions <- read.delim("/../MERGED_TRANSCRIPTOME/LRS_SRS_nofusion_junctions.txt", header=FALSE,  quote="'")
-LRS_SRS_junctions_coverage <- LRS_SRS_junctions %>% subset(select = c("isoform", "genomic_start_coord", "genomic_end_coord", "chrom", "strand", "total_coverage_unique"))
-LRS_SRS_junctions_coverage1 <- LRS_SRS_junctions_coverage %>%mutate(genomic_start_coord = genomic_start_coord-1) %>%   mutate(genomic_end_coord = genomic_end_coord+1)
+LRS_SRS_junctions_coverage <- LRS_SRS_junctions %>% subset(select = c("isoform", "genomic_start_coord", "genomic_end_coord", "chrom", "strand", "total_coverage_unique")) %>% LRS_SRS_junctions_coverage %>%mutate(genomic_start_coord = genomic_start_coord-1) %>%   mutate(genomic_end_coord = genomic_end_coord+1)
 
 #list of isoforms with at least 1 splice junction with less than 4 reads
-LRS_SRS_junctions_coverage_lessthan4 <- LRS_SRS_junctions_coverage1 %>% subset(total_coverage_unique <= 3) %>% subset(select = c("isoform")) %>% distinct()
+LRS_SRS_junctions_coverage_lessthan4 <- LRS_SRS_junctions_coverage %>% subset(total_coverage_unique <= 3) %>% subset(select = c("isoform")) %>% distinct()
 
 #list of isoforms with all splice junction with at least 4 reads
 LRS_SRS_alljncmorethan3 <- anti_join(LRS_SRS_junctions_coverage1,LRS_SRS_junctions_coverage_lessthan4)
 LRS_SRS_transcript_alljncmorethan3 <- LRS_SRS_alljncmorethan3  %>% subset(select = c("isoform")) %>% distinct()
 
+#gtf of isoforms with all splice junction with at least 4 reads
 LRS_SRS_gtf <- read.delim("/../MERGED_TRANSCRIPTOME/LRS_SRS_nofusion.gtf", header=FALSE,  quote="'")
 LRS_SRS_gtf_alljncmorethan3 <- LRS_SRS_gtf %>% separate(V9, into = c("V10", "V11", "V12"), sep = "; ", remove = FALSE) %>% mutate (V10 = gsub('transcript_id ', '', V10)) %>% mutate (V10 = gsub('"', '', V10)) %>%
   keeping.order(merge, y=LRS_SRS_transcript_alljncmorethan3, by.x = "V10", by.y = "isoform") 
 
+#filter out low expressed isoforms (less thant 4 reads in at least 1 splice-junction) from ensembl and refseq
+
+#table with splice junction coverage information
+ensembl_junctions <- read.delim("/../REFERENCE_INPUT/ensembl_junctions.txt", header=FALSE,  quote="'")
+refseq_junctions <- read.delim("/../REFERENCE_INPUT/refseq_junctions.txt", header=FALSE,  quote="'")
+ensembl_junctions_coverage <- ensembl_junctions %>% subset(select = c("isoform", "genomic_start_coord", "genomic_end_coord", "chrom", "strand", "total_coverage_unique")) %>%mutate(genomic_start_coord = genomic_start_coord-1) %>%   mutate(genomic_end_coord = genomic_end_coord+1)
+refseq_junctions_coverage <- refseq_junctions %>% subset(select = c("isoform", "genomic_start_coord", "genomic_end_coord", "chrom", "strand", "total_coverage_unique")) %>%mutate(genomic_start_coord = genomic_start_coord-1) %>%   mutate(genomic_end_coord = genomic_end_coord+1)
+
+#list of isoforms with at least 1 splice junction with less than 4 reads
+ensembl_junctions_coverage_lessthan4 <- ensembl_junctions_coverage1 %>% subset(total_coverage_unique <= 3) %>% subset(select = c("isoform")) %>% distinct()
+refseq_junctions_coverage_lessthan4 <- refseq_junctions_coverage1 %>% subset(total_coverage_unique <= 3) %>% subset(select = c("isoform")) %>% distinct()
+
+#list of isoforms with all splice junction with at least 4 reads
+ensembl_alljncmorethan3 <- anti_join(ensembl_junctions_coverage,ensembl_junctions_coverage_lessthan4)
+refseq_alljncmorethan3 <- anti_join(refseq_junctions_coverage,refseq_junctions_coverage_lessthan4)
+ensembl_transcript_alljncmorethan3 <- ensembl_alljncmorethan3  %>% subset(select = c("isoform")) %>% distinct()
+refseq_transcript_alljncmorethan3 <- refseq_alljncmorethan3  %>% subset(select = c("isoform")) %>% distinct()
+
+#gtf of isoforms with all splice junction with at least 4 reads
+ensembl_gtf <- read.delim("/../MERGED_TRANSCRIPTOME/ensembl_corrected.gtf", header=FALSE,  quote="'")
+refseq_gtf <- read.delim("/../MERGED_TRANSCRIPTOME/refseq_corrected.gtf", header=FALSE,  quote="'")
+ensembl_gtf_alljncmorethan3 <- ensembl_gtf %>% separate(V9, into = c("V10", "V11", "V12"), sep = "; ", remove = FALSE) %>% mutate (V10 = gsub('transcript_id ', '', V10)) %>% mutate (V10 = gsub('"', '', V10)) %>%
+  keeping.order(merge, y=ensembl_transcript_alljncmorethan3, by.x = "V10", by.y = "isoform") 
+refseq_gtf_alljncmorethan3 <- refseq_gtf %>% separate(V9, into = c("V10", "V11", "V12"), sep = "; ", remove = FALSE) %>% mutate (V10 = gsub('transcript_id ', '', V10)) %>% mutate (V10 = gsub('"', '', V10)) %>%
+  keeping.order(merge, y=refseq_transcript_alljncmorethan3, by.x = "V10", by.y = "isoform") 
+
 #list with AS coordinates
+
 NSC.NP.down <- read.delim("/../WHIPPET_RESULTS/NSC.NP.down.txt", header=FALSE,  quote="'")
 NP.N.down <- read.delim("/../WHIPPET_RESULTS/NP.N.down.txt", header=FALSE,  quote="'")
 NP.N.up <- read.delim("/../WHIPPET_RESULTS/NP.N.up.txt", header=FALSE,  quote="'")
@@ -78,14 +105,14 @@ as_not_start_and_or_end <-  NODES %>% subset(select = c("name")) %>% setdiff(as_
 
 #check which of the remaining AS events coordinates overlaps at least partially an exon
 #prepare file for bedtools
-as_not_start_and_or_end1 <- as_not_start_and_or_end %>% mutate(Zero = 0) %>% subset(select =c("Chr", "Start", "End", "name", "Zero","Strand")) %>%  mutate(Chr=gsub('^', 'chr', Chr))
-write.table(as_not_start_and_or_end1, "/../ISOFORM/as_not_start_and_or_end_coord.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)  
+as_not_start_and_or_end_coord <- as_not_start_and_or_end %>% mutate(Zero = 0) %>% subset(select =c("Chr", "Start", "End", "name", "Zero","Strand")) %>%  mutate(Chr=gsub('^', 'chr', Chr))
+write.table(as_not_start_and_or_end_coord, "/../ISOFORM/as_not_start_and_or_end_coord.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)  
 LRS_SRS_alljncmorethan3_coord <- LRS_SRS_gtf_alljncmorethan3 %>% separate(V9, into = c("V10", "V11", "V12"), sep = "; ", remove = FALSE) %>% mutate (V10 = gsub('transcript_id ', '', V10)) %>% mutate (V10 = gsub('"', '', V10)) %>% distinct() %>%
   separate(V9, into = c("a", "b", "c"), sep = ";"  ) %>% unite(V9, c("b", "a", "c"), sep = "; ") %>%  mutate(Chr=gsub('^', 'chr', V1))%>% mutate(Zero = 0) %>%
   subset(V3=="exon", select = c("Chr", "V4", "V5", "V9", "Zero", "V7"))
 write.table(LRS_SRS_alljncmorethan3_coord, "/../ISOFORM/LRS_SRS_alljncmorethan3_coord.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)  
 
-system("/../ISOFORM/bedtools_lrs_srs.sh")
+system("/../ISOFORM/bedtools_inclusion_lrs_srs.sh")
 
 #output from bedtools
 as_overlap <- read.delim("/../ISOFORM/as_overlap.txt", header=FALSE,  quote="'")
@@ -96,67 +123,37 @@ as_overlap <- as_overlap %>% subset(select = c("V1", "V4","V6", "V8", "V9", "V10
 as_not_overlap <-  NODES %>% subset(select = c("name")) %>% distinct()%>% setdiff(as_start_end_name) %>% setdiff(as_start_name) %>% setdiff(as_end_name) %>%setdiff(as_overlap_name) %>%merge(NODES, by="name") 
 
 #check if coordinate of the remaining AS events match coord from annotated transcripts
-#filter out low expressed isoforms (less thant 4 reads in at least 1 splice-junction)
 
-#table with splice junction coverage information
-ensembl_junctions <- read.delim("/../REFERENCE_INPUT/ensembl_junctions.txt", header=FALSE,  quote="'")
-refseq_junctions <- read.delim("/../REFERENCE_INPUT/refseq_junctions.txt", header=FALSE,  quote="'")
-ensembl_gtf <- read.delim("/../MERGED_TRANSCRIPTOME/ensembl_corrected.gtf", header=FALSE,  quote="'")
-refseq_gtf <- read.delim("/../MERGED_TRANSCRIPTOME/refseq_corrected.gtf", header=FALSE,  quote="'")
-
-ensembl_junctions_coverage <- ensembl_junctions %>% subset(select = c("isoform", "genomic_start_coord", "genomic_end_coord", "chrom", "strand", "total_coverage_unique")) %>%mutate(genomic_start_coord = genomic_start_coord-1) %>%   mutate(genomic_end_coord = genomic_end_coord+1)
-refseq_junctions_coverage <- refseq_junctions %>% subset(select = c("isoform", "genomic_start_coord", "genomic_end_coord", "chrom", "strand", "total_coverage_unique")) %>%mutate(genomic_start_coord = genomic_start_coord-1) %>%   mutate(genomic_end_coord = genomic_end_coord+1)
-
-#list of isoforms with at least 1 splice junction with less than 4 reads
-ensembl_junctions_coverage_lessthan4 <- ensembl_junctions_coverage1 %>% subset(total_coverage_unique <= 3) %>% subset(select = c("isoform")) %>% distinct()
-refseq_junctions_coverage_lessthan4 <- refseq_junctions_coverage1 %>% subset(total_coverage_unique <= 3) %>% subset(select = c("isoform")) %>% distinct()
-
-#list of isoforms with all splice junction with at least 4 reads
-ensembl_alljncmorethan3 <- anti_join(ensembl_junctions_coverage,ensembl_junctions_coverage_lessthan4)
-refseq_alljncmorethan3 <- anti_join(refseq_junctions_coverage,refseq_junctions_coverage_lessthan4)
-ensembl_transcript_alljncmorethan3 <- ensembl_alljncmorethan3  %>% subset(select = c("isoform")) %>% distinct()
-refseq_transcript_alljncmorethan3 <- refseq_alljncmorethan3  %>% subset(select = c("isoform")) %>% distinct()
-
-#gtf (exon coordinates)
-ensembl_gtf_alljncmorethan3 <- ensembl_gtf %>% separate(V9, into = c("V10", "V11", "V12"), sep = "; ", remove = FALSE) %>% mutate (V10 = gsub('transcript_id ', '', V10)) %>% mutate (V10 = gsub('"', '', V10)) %>%
-  keeping.order(merge, y=ensembl_transcript_alljncmorethan3, by.x = "V10", by.y = "isoform") 
-refseq_gtf_alljncmorethan3 <- refseq_gtf %>% separate(V9, into = c("V10", "V11", "V12"), sep = "; ", remove = FALSE) %>% mutate (V10 = gsub('transcript_id ', '', V10)) %>% mutate (V10 = gsub('"', '', V10)) %>%
-  keeping.order(merge, y=refseq_transcript_alljncmorethan3, by.x = "V10", by.y = "isoform") 
-
-#Check which of the remaining AS events have Start and End coordinates that match with both Start and End coordinate of an exon
+#check which of the remaining AS events match with both Start and End coordinate of an exon
 ens_startend <- ensembl_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V4","V5"), by.y = c("Start", "End")) %>%subset(select = c("V1", "V4","V5" ,"V7", "V9", "name"))
-#PB.9743-19, PB.9582-18,PB.9316-9, PB.8772-6,PB.2280-4,  PB.12316-82, PB.4866-14 - all ens
-refseq_startend <- refseq_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V4","V5"), by.y = c("Start", "End")) %>%subset(select = c("V1", "V4","V5" ,"V7", "V9", "name"))
-refseq_startend <- refseq_startend[grep('PB.4732-34|PB.1991-12', refseq_startendE$name), ]
-#PB.4732-34,PB.1991-12
+ens_startend_node <- ens_startend %>% subset(select = c("name")) %>% distinct()
+refseq_startend <- refseq_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V4","V5"), by.y = c("Start", "End")) %>%subset(select = c("V1", "V4","V5" ,"V7", "V9", "name")) %>% anti_join(ens_startend_node)
+refseq_startend_node <- refseq_startend %>% subset(select = c("name")) %>% distinct()
 
-#Check which of the remaining AS events have Start or End coordinates that match with either Start or End coordinate of an exon
-ens_start <- ensembl_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V4"), by.y = c("Start"))  %>% anti_join(ens_startend) %>% subset(select = c("V1", "V4","End" ,"V7", "V9", "name")) %>% dplyr::rename(V5=End)
-#PB.6167-3, PB.5031-60, PB.4800-6, PB.4866-7, PB.2781-9, PB.8482-32 - all ens
-ens_end <- ensembl_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V5"), by.y = c("End"))  %>% anti_join(ens_startend)%>% subset(select = c("V1", "Start","V5" ,"V7", "V9", "name")) %>% dplyr::rename(V4=Start)
-#PB.10632-10, PB.2400-2, PB.10531-13 
-ens_end <- ens_end[-grep('PB.4732-34', ens_end$name), ]
-refseq_start <- refseq_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V4"), by.y = c("Start"))  %>% anti_join(ens_startend) %>% subset(select = c("V1", "V4","End" ,"V7", "V9", "name")) %>% dplyr::rename(V5=End)
-refseq_start <- refseq_start[grep('PB.4052-20|PB.5326-30|PB.12524-23|PB.3724-21|PB.311-28|PB.1400-48|PB.449-3', refseq_start$name), ]
-#PB.4052-20|PB.5326-30|PB.12524-23|PB.3724-21|PB.311-28|PB.1400-48|PB.449-3
-refseq_end <- refseq_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V5"), by.y = c("End"))  %>% anti_join(ens_startend)%>% subset(select = c("V1", "Start","V5" ,"V7", "V9", "name")) %>% dplyr::rename(V4=Start)
-#PB.10942-29|PB.8463-5|PB.10400-12
-refseq_end <- refseq_end[grep('PB.10942-29|PB.8463-5|PB.10400-12', refseq_end$name), ]
+#check which of the remaining AS events have Start or End coordinates that match with either Start or End coordinate of an exon
+ens_start <- ensembl_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V4"), by.y = c("Start"))  %>% anti_join(ens_startend_node) %>% anti_join(refseq_startend_node) %>% subset(select = c("V1", "V4","End" ,"V7", "V9", "name")) %>% dplyr::rename(V5=End)
+ens_start_node <- ens_start %>% subset(select = c("name")) %>% distinct()
+ens_end <- ensembl_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V5"), by.y = c("End"))  %>% anti_join(ens_startend_node) %>% anti_join(refseq_startend_node)%>% subset(select = c("V1", "Start","V5" ,"V7", "V9", "name")) %>% dplyr::rename(V4=Start)
+ens_end_node <- ens_end %>% subset(select = c("name")) %>% distinct()
+refseq_start <- refseq_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V4"), by.y = c("Start")) %>% anti_join(ens_startend_node) %>% anti_join(refseq_startend_node) %>% anti_join(ens_start_node) %>% anti_join(ens_end_node) %>% subset(select = c("V1", "V4","End" ,"V7", "V9", "name")) %>% dplyr::rename(V5=End)
+refseq_start_node <- refseq_start %>% subset(select = c("name")) %>% distinct()
+refseq_end <- refseq_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>% merge(as_not_overlap, by.x = c("V5"), by.y = c("End")) %>% anti_join(ens_startend_node) %>% anti_join(refseq_startend_node) %>% anti_join(ens_start_node) %>% anti_join(ens_end_node) %>% subset(select = c("V1", "Start","V5" ,"V7", "V9", "name")) %>% dplyr::rename(V4=Start)
+refseq_end_node <- refseq_end %>% subset(select = c("name")) %>% distinct()
 
-#check which of the remaining AS events coordinates overlaps an exon
+#check which of the remaining AS events overlaps an exon at least partially an exon
 #prepare file for bedtools
+
+as_not_overlap_coord <- as_not_overlap %>% mutate(Zero = 0) %>% subset(select =c("Chr", "Start", "End", "name", "Zero","Strand")) %>%  mutate(Chr=gsub('^', 'chr', Chr))
+write.table(as_not_overlap_coord, "/../ISOFORM/as_not_overlap_coord.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)  
 ensembl_gtf_alljncmorethan3_coord <- ensembl_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>%  mutate(Chr=gsub('^', 'chr', V1))%>% mutate(Zero = 0) %>% subset(V3=="exon", select = c("Chr", "V4", "V5", "V9", "Zero", "V7"))
-write.table(ensembl_gtf_alljncmorethan3_coord, "/Beatriz_Toledo/nocapMerge/ensembl_gtf_alljncmorethan3_coord.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)  
+write.table(ensembl_gtf_alljncmorethan3_coord, "/../ISOFORM/ensembl_gtf_alljncmorethan3_coord.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)  
 refseq_gtf_alljncmorethan3_coord <- refseq_gtf_alljncmorethan3 %>% subset(V3 == "exon") %>%  mutate(Chr=gsub('^', 'chr', V1))%>% mutate(Zero = 0) %>% subset(V3=="exon", select = c("Chr", "V4", "V5", "V9", "Zero", "V7"))
-write.table(refseq_gtf_alljncmorethan3_coord, "/Beatriz_Toledo/nocapMerge/refseq_gtf_alljncmorethan3_coord.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)  
+write.table(refseq_gtf_alljncmorethan3_coord, "/../ISOFORM/refseq_gtf_alljncmorethan3_coord.txt", sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)  
 
-#on shell do bedtools intersect -a LRS_SRS_alljncmorethan3_coord.txt -b as_not_start_and_or_end_coord.txt -wo -s -F 0.99 -filenames > as_overlap.txt
-system("/../__/custom_script.sh")
-#bedtools intersect -a ensembl_gtf_alljncmorethan3_coord.txt -b NODES2022.merged.eventstocheckIR.txt -wo -s -F 0.99 -filenames > NODES2022.merged.overlatirensemblFilteredincluded.txt
-#bedtools intersect -a refseq_gtf_alljncmorethan3_coord.txt -b NODES2022.merged.eventstocheckIR.txt -wo -s -F 0.99 -filenames > NODES2022.merged.overlatirrefseqFilteredincluded.txt
+system("/../ISOFORM/bedtools_inclusion_ens_ref.sh")
 
-notmergedoverlatirensembl <- read.delim("/Beatriz_Toledo/nocapMerge/NODES2022.merged.overlatirensemblFilteredincluded.txt", header=FALSE)
-notmergedoverlatirrefseq <- read.delim("/Beatriz_Toledo/nocapMerge/NODES2022.merged.overlatirrefseqFilteredincluded.txt", header=FALSE)
+as_overlap_ens <- read.delim("/../ISOFORM/as_overlap_ens.txt", header=FALSE)
+as_overlap_ref <- read.delim("/../ISOFORM/as_overlap_ref.txt", header=FALSE)
 reftocheck <- merge(all.notmerged.simplenamecheck4, notmergedoverlatirrefseq, by.x = "name", by.y = "V10")
 reftocheck1<- reftocheck[grep('PB.11846-27|PB.4956-74', reftocheck$name), ] %>% subset(select = c("V1", "Start", "End", "Strand", "V4", "name"))
 #PB.11846-27|PB.4956-74
